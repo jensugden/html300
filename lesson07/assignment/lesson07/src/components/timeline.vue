d1488888ce6f3d965c9bfe95bb9045ec6c2b23f8
Jenny Sugden <jensugden@gmail.com>
2020-05-23 13:02:35 -0700

    working on html and vue scripts

---
+++ lesson07/assignment/lesson07/src/components/timeline.vue
@@ -0,0 +1,71 @@
+<template>
+    <div>
+      <h1 class="display-4 mt-3">Timeline</h1>
+      <div class="container-fluid">
+        <div class="row bg-info text-white lead">
+          <div class="col-xs-1 col-sm-1 col-md-1 col-lg-2">
+            Years
+          </div>
+          <div class="col-xs-1 col-sm-1 col-md-2 col-lg-2">
+            Genre Name
+          </div>
+          <div class="col-xs-1 col-sm-1 col-md col-lg">
+            Description
+          </div>
+        </div>
+      </div>
+
+      <!-- <div class="row pt-2 pb-2 bg-light">
+        <div v-for="era in eras" :key="era.year" class="col-xs-1 col-sm-1 col-md-1 col-lg-2">
+          {{eras.year}}
+        </div>
+        <div v-for="era in eras" :key="era.genre" class="col-xs-1 col-sm-1 col-md-2 col-lg-2">
+          {{eras.genre}}
+        </div>
+        <div v-for="era in eras" :key="era.descrip" class="col-xs-1 col-sm-1 col-md col-lg">
+          {{eras.descrip}}
+        </div>
+      </div> -->
+
+      <!-- <div id="test">
+        <li v-for="item in items" :key="item.message">
+          {{ item.message }}
+        </li>
+      </div> -->
+      <ul>
+        <li v-for="{person, index} in persons" :key="index">
+          {{person.name}}
+          {{person.age}}
+          {{person.color}}
+        </li>
+      </ul>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'timeline',
+  data () {
+    persons:[
+      {name: 'jenny', age: '45', color: 'yellow'},
+      {name: 'jones', age: '43', color: 'blue'}
+    ]
+    // return {
+    //   eras: [
+    //     {year: '1400-1600', genre: 'Renaissance', descrip: 'Natural elements, individualism, realism, attention-to-detail, precision of human anatomy'},
+    //     {year: '1527-1580', genre: 'Mannerism', descrip: 'Stylized features, exaggerated details, decorative elements'}
+    //   ]
+    // }
+  }
+}
+
+// new Vue({
+//   el: '#test',
+//   data: {
+//     items: [
+//       {message: 'foo'},
+//       {message: 'bar'}
+//     ]
+//   }
+// })
+</script>
--- lesson07/assignment/lesson07/src/views/timeline.vue
+++ lesson07/assignment/lesson07/src/views/timeline.vue
@@ -1,97 +1,18 @@
 <template>
-  <div class="timeline">
-    <h1>Timeline</h1>
-    <!-- <div class="test"></div> -->
+  <div>
+    <timeline/>
+
   </div>
 </template>
 
-// <script>
-export default {
-  data () {
+<script>
+import timeline from '../components/timeline.vue'
 
+export default {
+  name: 'app',
+  components: {
+    timeline
   }
 }
-// $(document).ready(function(){
-//
-//   const eras = [
-//   {
-//     "year": "1400-1600",
-//     "genre": "Renaissance",
-//     "description": "Natural elements, individualism, realism, attention-to-detail, precision of human anatomy",
-//   },
-//   {
-//     "year": "1527-1580",
-//     "genre": "Mannerism",
-//     "description": "Stylized features, exaggerated details, decorative elements",
-//   },
-//   {
-//     "year": "1600-1750",
-//     "genre": "Baroque",
-//     "description": "Ornate, grandeur, richness, stylistically complex, dramatic",
-//   },
-//   {
-//     "year": "1699-1780",
-//     "genre": "Rococo",
-//     "description": "Lightness, elegance, natural forms, asymmetrical design, subtle colors",
-//   },
-//   {
-//     "year": "1750-1850",
-//     "genre": "Neoclassicism",
-//     "description": "Renewed interest in classical antiquity, harmony, simplicity, and proportion",
-//   },
-//   {
-//     "year": "1780-1850",
-//     "genre": "Romanticim",
-//     "description": "Imaginative elements, focus on passion, emotion, and observing the senses",
-//   },
-//   {
-//     "year": "1848-1900",
-//     "genre": "Realism",
-//     "description": "Detailed depictions of daily life",
-//   },
-//   {
-//     "year": "1890-1910",
-//     "genre": "Art Nouveau",
-//     "description": "Long, sinuous lines and curves",
-//   },
-//
-//   function getCard(identity) {
-//     let Card = `
-//       <div class="timeline row pt-2 pb-2">
-//         <div class="col-xs-1 col-sm-1 col-md-1 col-lg-2">
-//           ${identity.year}
-//         </div>
-//         <div class="col-xs-1 col-sm-1 col-md-2 col-lg-2">
-//           ${identity.genre}</h2>
-//         </div>
-//         <div class="col-xs-1 col-sm-1 col-md col-lg">
-//           ${identity.genre}</h2>
-//         </div>
-//       </div>
-//     `;
-//     return Card;
-//   },
-//
-//   //run loop over all objects
-//   const demo = eras.map(function(getCard){
-//     let Card = `<div class="timeline row pt-2 pb-2">
-//       <div class="col-xs-1 col-sm-1 col-md-1 col-lg-2">
-//         ${identity.year}
-//       </div>
-//       <div class="col-xs-1 col-sm-1 col-md-2 col-lg-2">
-//         ${identity.genre}</h2>
-//       </div>
-//       <div class="col-xs-1 col-sm-1 col-md col-lg">
-//         ${identity.genre}</h2>
-//       </div>
-//     </div>
-//       `;
-//       return Card;
-//     });
-//
-//     //append objects to HTML
-//     $(".test").append(demo);
-//   });
-// }
-//
-// </script>
+
+</script>
