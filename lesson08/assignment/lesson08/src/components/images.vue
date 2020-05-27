baad7ab512040458b3430d9e729473779060c14b
Jenny Sugden <jensugden@gmail.com>
2020-05-27 16:11:49 -0700

    started lesson 08 and recreated the images and gallery pages

---
+++ lesson08/assignment/lesson08/src/components/images.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+      <h1 class="display-4 mt-3">Gallery</h1>
+
+      <div class="flex" >
+        <img class="img-thumbnail img-fluid" v-for="pic in pics" :key="pic.id" :src="pic.url" :alt="pic.alt"/>
+      </div>
+
+    </div>
+</template>
+
+<script>
+
+export default {
+  props: {
+    pics: {
+      type: Array,
+      required: true,
+    }
+  }
+}
+</script>
+
+<style scoped>
+.flex {
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+}
+
+img {
+  margin: .25rem;
+}
+</style>
---
+++ lesson08/assignment/lesson08/src/views/gallery.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+
+    <images v-bind:pics="pics"></images>
+
+  </div>
+</template>
+
+<script>
+import images from '../components/images.vue'
+// import image1 from "../assets/Esther_before_Ahasuerus.jpg"
+// import image2 from "../assets/375px-Girl_with_Cherries.jpg"
+// import image3 from "../assets/299px-Bianca_Maria_Sforza.jpg"
+// import image4 from "../assets/The_Day_Dream.jpg"
+// import image5 from "../assets/350px-Portrait_of_a_Lady.jpg"
+
+
+export default {
+  components: {
+    images
+  },
+  data: function (){
+    return {
+      pics: [
+        {url: require('../assets/Esther_before_Ahasuerus.jpg'), alt: 'Esther before Ahasuerus'},
+        {url: require('../assets/375px-Girl_with_Cherries.jpg'), alt: 'Girl with Cherries'},
+        {url: require('../assets/299px-Bianca_Maria_Sforza.jpg'), alt: 'Bianca Maria Sforza'},
+        {url: require('../assets/The_Day_Dream.jpg'), alt: 'The Day Dream'},
+        {url: require('../assets/350px-Portrait_of_a_Lady.jpg'), alt: 'Portrait of a Lady'},
+      ]
+    }
+  }
+}
+</script>
