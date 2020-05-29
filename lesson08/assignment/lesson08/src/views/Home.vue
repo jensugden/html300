42ff38b2005b919fd6088b9299b0968183f7282a
Jenny Sugden <jensugden@gmail.com>
2020-05-29 10:54:38 -0700

    cleaned up home file

--- lesson08/assignment/lesson08/src/views/Home.vue
+++ lesson08/assignment/lesson08/src/views/Home.vue
@@ -1,15 +1,10 @@
 <template>
   <div >
     <landing/>
-
-    <!-- <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-// import HelloWorld from '@/components/HelloWorld.vue'
 import landing from '../components/landing.vue'
 
 export default {
