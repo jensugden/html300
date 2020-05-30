acfc23f99d9e5bea0aa2359e80be9f5b2cf15bcd
Jenny Sugden <jensugden@gmail.com>
2020-05-30 14:30:57 -0700

    completed slots

--- lesson08/assignment/lesson08/src/components/like-us.vue
+++ lesson08/assignment/lesson08/src/components/like-us.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="layout container-fluid">
-      <h1 class="display-4 mt-3" v-if="show">{{ message | to-uppercase }}</h1>
-      <h1 class="display-4 mt-3" v-else>Thanks!</h1>
-      <button class="btn-lg btn-success mb-3" v-on:click="show = !show">Click Here</button>
+      <h1 class="background display-4 mt-3" v-if="show">{{ message | to-uppercase }}</h1>
+      <h1 class="background display-4 mt-3" v-else>Thanks!</h1>
+      <button class="btn-lg btn-warning mb-3" v-on:click="show = !show">Click Here</button>
     </div>
 </template>
 
@@ -24,5 +24,9 @@ export default {
 </script>
 
 <style>
-
+.background {
+  background-color: rgba(0, 0, 0, 0.10);
+  padding: 1.5rem;
+  margin: 2rem;
+}
 </style>
